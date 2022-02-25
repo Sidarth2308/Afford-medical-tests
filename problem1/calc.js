c1e25d458892c1d241187515a2063f11a0af063b
sidthunder007 <sidthunder007@gmail.com>
2022-02-25 11:35:15 +0530

    completed problem 1

---
+++ problem1/calc.js
@@ -0,0 +1,9 @@
+function calculator(data) {
+  let sortedData = data.sort(function (a, b) {
+    return +a - +b;
+  });
+  uniq = [...new Set(sortedData)];
+  return uniq;
+}
+
+module.exports = calculator;
